3ed57e2a3076ec2198a0c8bb8870207caca0e394
steve macharia <40029452+stevemacharia@users.noreply.github.com>
2022-06-27 16:45:20 +0300

    Update settings.py

--- kutrrhsips/kutrrhsips/settings.py
+++ kutrrhsips/kutrrhsips/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'NAME' : 'kutrrhsips',
-        'USER' : 'sips',
+        'USER' : 'kutrrhsips',
         'PASSWORD' : 'sips@2022!#',
         'HOST' : 'localhost',
         'PORT' : '3306',
